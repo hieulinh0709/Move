8dc9ddb3d98a0cd7868e4f56650c372287bac3c7
waiting <hieulinh512@gmail.com>
2021-09-20 08:30:13 +0700

    move

---
+++ D365.Lab/ConsoleDebug/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Net;
+using System.Text;
+
+namespace ConsoleDebug
+{
+    class Program
+    {
+        private static IOrganizationService _service;
+
+        static void Main(string[] args)
+        {
+
+            var client = new CrmServiceClient(connectionString);
+            _service = client.OrganizationWebProxyClient;
+
+            var userid = ((WhoAmIResponse)_service.Execute(new WhoAmIRequest()))?.UserId ?? Guid.Empty;
+
+            if (userid.Equals(Guid.Empty))
+            {
+                Console.WriteLine("Connected error");
+            }
+            else
+            {
+                Console.WriteLine("Connected");
+
+                //Console.WriteLine("******************* entity0 *******************");
+                //var json = new JavaScriptSerializer().Serialize(entity0);
+                //Console.WriteLine(json);
+                var program = new PluginValidator.Program("dg", "gdfg");
+                program.Main(_service, accountEntity);
+            }
+
+            Console.WriteLine("End");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ D365.Lab/PluginValidator/Configurations/Phone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginValidator.Configurations
+{
+    public class Phone
+    {
+        public string Province { get; set; }
+        public string ProvinceCode { get; set; }
+    }
+}
