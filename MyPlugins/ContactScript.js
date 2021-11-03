deac2e6c0a481d393b6c4ffd3aa4b5059e4f6569
waiting <hieulinh512@gmail.com>
2021-11-04 00:13:28 +0700

    dynamics 365

---
+++ MyPlugins/ContactScript.js
@@ -0,0 +1,11 @@
+﻿var Sdk = window.Sdk || {};
+(
+    function () {
+        this.formOnLoad = function (executionContext) {
+            var formContext = executionContext.getFormContext();
+            var firstName = formContext.getAttribute("firstname").getValue();
+            alert("Hello: " + firstName);
+        }
+    }
+
+).call(Sdk);
